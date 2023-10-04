f04f6bf59d2149d53ade17d36c991aa1193e91c1
sunilcostacloud <sunil@costacloud.com>
2023-10-04 17:54:34 +0530

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,12 @@ import styles from "./Header.module.css";
 const Header = () => {
     return (
         <div className={styles.HeaderParent} >
-            Header
+            <div className={styles.heading}>
+                Dashboard
+            </div>
+            <div>
+
+            </div>
         </div>
     )
 }
